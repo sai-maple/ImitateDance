e7db6193d81e5443155504661a07d633075b0473
hiroaki.goto <hiroakigotou1229@gmail.com>
2022-05-03 13:22:12 +0900

    タイマー修正

--- Assets/ImitateDunce/Scripts/Domain/Entity/TimeEntity.cs
+++ Assets/ImitateDunce/Scripts/Domain/Entity/TimeEntity.cs
@@ -1,15 +1,24 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+
 namespace ImitateDunce.Domain.Entity
 {
     public sealed class TimeEntity
     {
         public float Time { get; private set; }
 
-        public void Start()
+        public async UniTask StartAsync(float limit, CancellationToken token)
         {
             Time = 0;
+            while (true)
+            {
+                await UniTask.Yield(PlayerLoopTiming.FixedUpdate, token);
+                if (token.IsCancellationRequested) return;
+                if (Time < limit) break;
+            }
         }
 
-        public void Update(float deltaTime)
+        public void FixUpdate(float deltaTime)
         {
             Time += deltaTime;
         }
