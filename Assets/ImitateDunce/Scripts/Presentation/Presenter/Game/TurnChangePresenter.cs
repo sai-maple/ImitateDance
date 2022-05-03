12afc2574b259b03a36c2067ada32b11e1ac4420
hiroaki.goto <hiroakigotou1229@gmail.com>
2022-05-03 19:12:58 +0900

    TurnChangeのPresenter仮置き

---
+++ Assets/ImitateDunce/Scripts/Presentation/Presenter/Game/TurnChangePresenter.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Threading;
+using ImitateDunce.Applications.Enums;
+using ImitateDunce.Domain.Entity.Game.Core;
+using ImitateDunce.Domain.UseCase.Game.Core;
+using UniRx;
+using VContainer.Unity;
+
+namespace ImitateDunce.Presentation.Presenter.Game
+{
+    public sealed class TurnChangePresenter : IInitializable, IDisposable
+    {
+        private readonly PhaseEntity _phaseEntity = default;
+        private readonly TurnUseCase _turnUseCase = default;
+
+        private readonly CompositeDisposable _disposable = new CompositeDisposable();
+        private readonly CancellationTokenSource _cancellationToken = new CancellationTokenSource();
+
+        public void Initialize()
+        {
+            _phaseEntity.OnChangeAsObservable()
+                .Where(phase => phase == DuncePhase.TurnChange)
+                .Subscribe(_ =>
+                {
+                    // todo viewの状態変化, 歓声の再生
+                    _turnUseCase.OnTurnChange(_cancellationToken.Token);
+                })
+                .AddTo(_disposable);
+        }
+
+        public void Dispose()
+        {
+            _disposable.Dispose();
+            _cancellationToken.Cancel();
+            _cancellationToken.Dispose();
+        }
+    }
+}
