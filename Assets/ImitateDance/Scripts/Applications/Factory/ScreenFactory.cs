2b2c403c86e0a7c0382fd85c332bcb3ed40b4df0
hiroaki.goto <hiroakigotou1229@gmail.com>
2022-05-05 01:15:08 +0900

    Screenの生成修正対応

--- Assets/ImitateDance/Scripts/Applications/Factory/ScreenFactory.cs
+++ Assets/ImitateDance/Scripts/Applications/Factory/ScreenFactory.cs
@@ -8,11 +8,11 @@ namespace ImitateDance.Scripts.Applications.Factory
 {
     public sealed class ScreenFactory : UniScreen.Factory.ScreenFactory
     {
-        public override async UniTask<ScreenView> CreateAsync(string screen, CancellationToken token)
+        public override async UniTask<ScreenView> CreateAsync(string screen, Transform content, CancellationToken token)
         {
             // var asset = (ScreenView)await Resources.LoadAsync<ScreenView>(screen);
             var asset = await Addressables.LoadAssetAsync<GameObject>(screen);
-            return asset.GetComponent<ScreenView>().Create();
+            return asset.GetComponent<ScreenView>().Create(content);
         }
     }
 }
