ee3d6c6f322219c8c938830d44da421bc6f6d2c9
hiroaki.goto <hiroakigotou1229@gmail.com>
2022-05-07 00:05:25 +0900

    メニュー、タイトル、ロードのアニメーション追加

---
+++ Assets/ImitateDance/Scripts/Presentation/View/Common/CanvasHideAwake.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace ImitateDance.Scripts.Presentation.View.Common
+{
+    public sealed class CanvasHideAwake : MonoBehaviour
+    {
+        private void Awake()
+        {
+            if (!TryGetComponent<CanvasGroup>(out var canvasGroup)) return;
+            canvasGroup.alpha = 0;
+        }
+    }
+}
