9caace5d1002058a66d85e38ead18ea3b12647d9
hiroaki.goto <hiroakigotou1229@gmail.com>
2022-05-21 22:13:12 +0900

    設定のデザイン調整

--- Assets/ImitateDance/Scripts/Presentation/View/Screen/PopupScreenAnimation.cs
+++ Assets/ImitateDance/Scripts/Presentation/View/Screen/PopupScreenAnimation.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using DG.Tweening;
@@ -10,13 +11,16 @@ namespace ImitateDance.Scripts.Presentation.View.Screen
     {
         [SerializeField] private RectTransform _content = default;
         [SerializeField] private CanvasGroup _canvas = default;
+        [SerializeField] private float _dilay = default;
 
         public override async UniTask Show(CancellationToken token)
         {
             if (IsShow) return;
             _content.localScale = new Vector3(1, 0, 1);
             _canvas.alpha = 0;
-            await _content.DOScale(Vector3.one, 0.2f).WithCancellation(token);
+            if (_dilay != 0) await UniTask.Delay(TimeSpan.FromSeconds(_dilay), cancellationToken: token);
+            if (token.IsCancellationRequested) return;
+            await _content.DOScale(Vector3.one, 0.3f).WithCancellation(token);
             if (token.IsCancellationRequested) return;
             await _canvas.DOFade(1, 0.2f).WithCancellation(token);
             IsShow = true;
@@ -27,7 +31,7 @@ namespace ImitateDance.Scripts.Presentation.View.Screen
             if (!IsShow) return;
             await _canvas.DOFade(0, 0.2f).WithCancellation(token);
             if (token.IsCancellationRequested) return;
-            await _content.DOScale(new Vector3(1,0,1), 0.2f).WithCancellation(token);
+            await _content.DOScale(new Vector3(1, 0, 1), 0.2f).WithCancellation(token);
             IsShow = false;
         }
     }
